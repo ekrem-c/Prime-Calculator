8dfc59af5bcd2b2ac01b5da74a3bf9d382f8f34c
ekrem-c <ekremcologlu@gmail.com>
2021-06-19 20:14:45 -0500

    all test cases are passing

--- PrimeCalculator/Program.cs
+++ PrimeCalculator/Program.cs
@@ -7,6 +7,7 @@ namespace PrimeCalculator
         public static bool isPrime(int number)
         {
             if (number < 2) return false;
+            if (number == 2) return true;
             if (number % 2 == 0) return false;
 
 
