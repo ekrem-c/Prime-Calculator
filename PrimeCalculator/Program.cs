6c9d6770fe99a8f0ebbedc3f8b3d4e9f24c3e1f7
ekrem-c <ekremcologlu@gmail.com>
2021-06-19 19:34:34 -0500

    initial commit

--- PrimeCalculator/Program.cs
+++ PrimeCalculator/Program.cs
@@ -4,9 +4,15 @@ namespace PrimeCalculator
 {
     class Program
     {
+        public static bool isPrime(int number)
+        {
+            if (number < 2) return false;
+
+            return true;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(isPrime(5));
         }
     }
 }
